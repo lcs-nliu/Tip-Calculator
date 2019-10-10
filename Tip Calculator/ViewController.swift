cddfac798f099d45981101938b191b56d6e052c7
Nicole Liu <nicoleliu_@outlook.com>
2019-10-10 11:17:20 -0400

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Liu, Bojun on 2019-10-10.
+//  Copyright © 2019 Liu, Nicole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
