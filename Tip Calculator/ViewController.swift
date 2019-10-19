0d1a84027df61c555f1a4a72e368fedd1a8e92c9
Nicole Liu <nicoleliu_@outlook.com>
2019-10-18 23:20:30 -0400

    edited the code and tried to troubleshoot

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -7,25 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    
     // MARK: Properties
     
-    //Establish Tip Amount
-    var tipPercent = Double(0.0)
     
-    var buttonPressed = false
+    //Establish Button Pressed as a false boolean
+    var buttonPressed: Bool = false
+    var tipPercent = Double (0.0)
     
+    // Connected text field of bill amount to controller using an outlet
+    @IBOutlet weak var amountOfBillEntered: UITextField!
+    
+    // Connected text field of people to controller using an outlet
+    
+    @IBOutlet weak var amountOfPeopleEntered: UITextField!
+    
+
+    // Connected total label to controller using an outlet
+    @IBOutlet weak var totalTipLabel: UILabel!
+    
+    // Connected tip per person label to controller using an outlet
+    @IBOutlet weak var tipPerPersonLabel: UILabel!
+    
+    // Connected error label to controller using an outlet
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+    
+    // MARK: Methods
+    
+    // Runs as soon as the view becomes visible to the user
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    // Connected tip buttons to controller using actions
+
     @IBAction func tip5(_ sender: Any) {
         tipPercent = 0.05
         buttonPressed = true
     }
     
     @IBAction func tip10(_ sender: Any) {
-        tipPercent = 0.1
+        tipPercent = 0.10
         buttonPressed = true
-        
     }
     
     @IBAction func tip15(_ sender: Any) {
@@ -34,74 +65,71 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tip20(_ sender: Any) {
-        tipPercent = 0.2
+        tipPercent = 0.20
         buttonPressed = true
     }
-    
-    // Establish Bill Amount
-    @IBOutlet weak var amountOfBill: UITextField!
-    
-    // Establish Amount of People
-    @IBOutlet weak var amountOfPeople: UITextField!
-    
-    // Total Tip Label
-    @IBOutlet weak var totalTipLabel: UILabel!
-    
-    // Tip Per Person Label
-    @IBOutlet weak var tipPerPersonLabel: UILabel!
-    
-  // Error Label
-    @IBOutlet weak var errorLabel: UILabel!
-    
-    
-    //MARK: Methods
-    
-
-    @IBAction func calculate(_sender: Any) {
-
+ 
+     //  Obtain the amount of bill value from the text field
+    @IBAction func calculateTip(_ sender: Any) {
       
-        
-        // Take strings and turn them into doubles
-        
-        // If the string can't be converted to a double, diplay message to user
-        guard let billAmountDouble = Double(amountOfBill) else{
-            errorLabel.text = "Enter Usable Values"
+    // Check if a tip option is selected
+        if buttonPressed == false {
+            errorLabel.text = "Error. Please check all fields are completed."
             return
         }
+    
+    // Check if amount of bill has a value
+        guard let amountOfBillString = amountOfBillEntered.text, amountOfBillString != ""  else {
+            errorLabel.text = "Error. Please check all fields are completed."
+            return
+    }
         
-        // If the string can't be converted to a double, diplay message to user
-        guard let amountOfPeopleAsDouble = Double(amountOfPeople) else{
-            errorLabel.text = "Enter Usable Values"
+    // Check if amount of people has a value
+        guard let amountOfPeopleString = amountOfPeopleEntered.text, amountOfPeopleString != ""  else {
+            errorLabel.text = "Error. Please check all fields are completed."
             return
         }
+
+    // Try to convert the string into an double data type display error message if cannot be converted
+            guard let amountOfBillDouble = Double(amountOfBillString) else {
+            errorLabel.text = "Error. Please enter a number."
+                return
+            }
+            
+    /* guard let amountOfPeopleDouble = Double(amountOfPeopleString) else {
+            //   errorLabel.text = "Error. Please enter a number."
+            //   return
+    } */
+      
         
         
         // Calculate tip amount
-        let tipAmountDouble = billAmountDouble * tipPercent
-        
+        let tipAmount = amountOfBillDouble * tipPercent
+
         // Round the value to 2 decimal places
-        let roundedtipAmountDouble = (round(100*tipAmountDouble)/100)
+        let roundedtipAmount = (round(100*tipAmount)/100)
         
         // Change tip amount into a string
-        let tipAmountInDollarsasString = String(roundedtipAmountDouble)
+        let tipAmountString = String(roundedtipAmount)
         
         // Share information to the user interface
-        totalTipLabel.text = "Total Tip: \(tipAmountInDollarsasString)$"
+        totalTipLabel.text = "Total Tip: \(tipAmountString)$"
         
         // Calculate tip per person
-        let tipAmountPerPerson = tipAmountDouble/amountOfPeopleAsDouble
+        let tipAmountPerPerson = tipAmount/2
         
         // Round the value to 2 decimal places
-        let roundedTipPerPerson = (round(100*tipAmountPerPerson)/100)
+        let roundedTipAmountPerPerson = (round(100*tipAmountPerPerson)/100)
         
         // Change tip per person into a string
-        let tipPerPersonasString = String(roundedTipPerPerson)
+        let roundedTipAmountPerPersonString = String(roundedTipAmountPerPerson)
         
         // Share information to the user interface
-        tipPerPersonLabel.text = "Tip Per Person: \(tipPerPersonasString)$"
+        tipPerPersonLabel.text = "Tip Per Person: \(roundedTipAmountPerPersonString)$"
         
         
-    }
+
     
 }
 
+}
